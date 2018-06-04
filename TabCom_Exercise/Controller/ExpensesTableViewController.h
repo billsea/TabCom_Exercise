6167b87cacfa0376efef8ce744f6f85d2c8ace2d
Bill S <bill.e.seaman@gmail.com>
2018-06-04 14:22:43 -0700

    Update model classes

---
+++ TabCom_Exercise/Controller/ExpensesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ExpensesTableViewController.h
+//  TabCom_Exercise
+//
+//  Created by Loud on 6/4/18.
+//  Copyright © 2018 Bill_Seaman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExpensesTableViewController : UITableViewController
+
+@end
