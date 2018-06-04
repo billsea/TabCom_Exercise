e1ebae596c9168d801c73d52f160f9ea1644a193
Bill S <bill.e.seaman@gmail.com>
2018-06-04 14:16:04 -0700

    Add basic data request

---
+++ TabCom_Exercise/Request/RequestData.h
@@ -0,0 +1,13 @@
+//
+//  RequestData.h
+//  TabCom_Exercise
+//
+//  Created by Loud on 6/4/18.
+//  Copyright © 2018 Bill_Seaman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RequestData : NSObject
+
+@end
---
+++ TabCom_Exercise/Request/RequestData.m
@@ -0,0 +1,33 @@
+//
+//  RequestData.m
+//  TabCom_Exercise
+//
+//  Created by Loud on 6/4/18.
+//  Copyright © 2018 Bill_Seaman. All rights reserved.
+//
+
+#import "RequestData.h"
+#import "Global.h"
+#import <AFNetworking/AFNetworking.h>
+
+@implementation RequestData
+
+- (void)requestMealDataWithResource:(NSString*)resourceName {
+	NSString* urlString = [NSString stringWithFormat:@"%@%@?format=json", BaseURLString, resourceName];
+	NSURL* dataUrl = [NSURL URLWithString:urlString];
+	
+	NSURLSessionConfiguration* requestConfig = [NSURLSessionConfiguration defaultSessionConfiguration];
+	AFURLSessionManager* requestManager = [[AFURLSessionManager alloc] initWithSessionConfiguration:requestConfig];
+	NSURLRequest* urlRequest = [NSURLRequest requestWithURL:dataUrl];
+	
+	//TODO : Log download progress
+	NSURLSessionDataTask* dataRequestTask = [requestManager dataTaskWithRequest:urlRequest uploadProgress:nil downloadProgress:nil completionHandler:^(NSURLResponse *response, id responseObject, NSError *error) {
+		if (error) {
+			NSLog(@"Error: %@", error);
+		} else {
+			NSLog(@"%@ %@", response, responseObject);
+		}
+	}];
+	[dataRequestTask resume];
+}
+@end
