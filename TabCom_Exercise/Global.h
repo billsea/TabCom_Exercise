88c5413bb46cfd8a86be129df1f85487d8b960e4
Bill S <bill.e.seaman@gmail.com>
2018-06-07 11:41:18 -0700

    Unignore the Global file

---
+++ TabCom_Exercise/Global.h
@@ -0,0 +1,13 @@
+//
+//  Global.h
+//  TabCom_Exercise
+//
+//  Created by Loud on 6/4/18.
+//  Copyright © 2018 Bill_Seaman. All rights reserved.
+//
+
+#ifndef Global_h
+#define Global_h
+
+#define BaseURLString @"https://api.tabletcommand.com/interview/"
+#endif /* Global_h */
